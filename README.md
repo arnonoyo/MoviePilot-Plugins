623b583a00eea8fcca129df0c410e8b950e90771
tK <18019369+arnonoyo@users.noreply.github.com>
2024-05-20 10:47:13 +0800

    feat: 转发webhook通知插件

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MoviePilot-Plugins
 1. 转种插件
 - 基于官方插件TorrentTransfer二次开发，支持自定义下载器
+2. 转发webhook通知插件
+- 基于SynologyNotify二次开发，支持自定义通知标题
---
+++ plugins/transferhooknotify/__init__.py
@@ -0,0 +1,178 @@
+from app.plugins import _PluginBase
+from typing import Any, List, Dict, Tuple
+from app.log import logger
+from app.schemas import NotificationType
+from app import schemas
+
+class TransferHookNotify(_PluginBase):
+    # 插件名称
+    plugin_name = "转发webhook通知"
+    # 插件描述
+    plugin_desc = "接收webhook通知并推送。(基于SynologyNotify二次开发)"
+    # 插件图标
+    plugin_icon = "webhook.png"
+    # 插件版本
+    plugin_version = "1.0"
+    # 插件作者
+    plugin_author = "thsrite,tk"
+    # 作者主页
+    author_url = "https://github.com/arnonoyo"
+    # 插件配置项ID前缀
+    plugin_config_prefix = "transferhooknotify_"
+    # 加载顺序
+    plugin_order = 30
+    # 可使用的用户级别
+    auth_level = 1
+
+    # 任务执行间隔
+    _enabled = False
+    _notify = False
+
+    def init_plugin(self, config: dict = None):
+        if config:
+            self._enabled = config.get("enabled")
+            self._notify = config.get("notify")
+
+    def send_notify(self, text: str, title: str = 'webhook通知') -> schemas.Response:
+        """
+        发送通知
+        """
+        logger.info(f"webhook: {text}")
+        if self._enabled:
+            self.post_message(title=title,
+                              mtype=NotificationType.Manual,
+                              text=text)
+
+        return schemas.Response(
+            success=True,
+            message="发送成功"
+        )
+
+    def get_state(self) -> bool:
+        return self._enabled
+
+    @staticmethod
+    def get_command() -> List[Dict[str, Any]]:
+        pass
+
+    def get_api(self) -> List[Dict[str, Any]]:
+        """
+        获取插件API
+        [{
+            "path": "/xx",
+            "endpoint": self.xxx,
+            "methods": ["GET", "POST"],
+            "summary": "API说明"
+        }]
+        """
+        return [{
+            "path": "/webhook",
+            "endpoint": self.send_notify,
+            "methods": ["GET"],
+            "summary": "转发Webhook通知",
+            "description": "接收webhook通知并推送",
+        }]
+
+    def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
+        """
+        拼装插件配置页面，需要返回两块数据：1、页面配置；2、数据结构
+        """
+        return [
+            {
+                'component': 'VForm',
+                'content': [
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 6
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSwitch',
+                                        'props': {
+                                            'model': 'enabled',
+                                            'label': '启用插件',
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 6
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSwitch',
+                                        'props': {
+                                            'model': 'notify',
+                                            'label': '开启通知',
+                                        }
+                                    }
+                                ]
+                            },
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VAlert',
+                                        'props': {
+                                            'type': 'info',
+                                            'variant': 'tonal',
+                                            'text': 'webhook配置http://ip:3001/api/v1/plugin/TransferHookNotify/webhook?text=hello world。'
+                                                    'text参数类型是消息内容。此插件安装完需要重启生效api。消息类型默认为手动处理通知。'
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VAlert',
+                                        'props': {
+                                            'type': 'info',
+                                            'variant': 'tonal',
+                                            'text': '如安装完插件后，发送webhook提示404，重启MoviePilot即可。'
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ], {
+            "enabled": False,
+            "notify": False
+        }
+
+    def get_page(self) -> List[dict]:
+        pass
+
+    def stop_service(self):
+        """
+        退出插件
+        """
+        pass
